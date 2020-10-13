12f527edf57d23bd9458fe1b8a4c3543ca02500a
COCCHI Simon <59911613+simoncocchi@users.noreply.github.com>
2020-10-13 16:33:02 +0200

    fix(gulpfile): use functions and exports for gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,13 @@
 const gulp = require("gulp");
 const imagemin = require("gulp-imagemin");
 const autoprefixer = require("gulp-autoprefixer");
+const { parallel } = require("gulp");
 
-exports.default = () => (
-  gulp.src("images/*").pipe(imagemin()).pipe(gulp.dest("dist/images")),
+function reduceImage(cb) {
+  gulp.src("images/*").pipe(imagemin()).pipe(gulp.dest("dist/images")), cb();
+}
+
+function cssModify(cb) {
   gulp
     .src("styles.css")
     .pipe(
@@ -11,5 +15,10 @@ exports.default = () => (
         cascade: false,
       })
     )
-    .pipe(gulp.dest("dist"))
-);
+    .pipe(gulp.dest("dist"));
+  cb();
+}
+
+exports.cssModify = cssModify;
+exports.reduceImage = reduceImage;
+exports.default = parallel(reduceImage, cssModify);
